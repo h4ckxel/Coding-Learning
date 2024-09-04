4854b515c2ea86730b7508d7cbe232b97f6573c2
h4ckxel <acxel.programming@gmail.com>
2024-09-04 05:25:11 -0600

    ComplejidadConstante.py

--- ComplejidadConstante_3_9_2024.py
+++ ComplejidadConstante_3_9_2024.py
@@ -9,22 +9,22 @@ Created on Tue Sep  3 11:07:18 2024
 
 import time
 
-def sumita(n):
-    juguito = 0
+def suma(n):
+    zuma = 0
     for i in range(1, n+1):
-        juguito += 1
-    return juguito
+        zuma += 1
+    return zuma
 
-def sumatota(n):
+def bigsuma(n):
     return n*(n+1) // 2
 
 n = 5
 
 # medicion del tiempo
 inicio = time.time()
-suma1 = sumita(n)
+suma1 = suma(n)
 fin = time.time()
 print(f"SUMA ITERATIVA: {suma1}, TIEMPO: {fin - inicio:.10f} segundos")
 
-print(f"La suma de los primeros {n} numero es: {sumita(n)}")
+print(f"La suma de los primeros {n} numero es: {suma(n)}")
 
