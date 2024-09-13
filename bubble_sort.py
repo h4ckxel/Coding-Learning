e104d729f02493930b31ee09c92ce69c537bdcc8
h4ckxel <acxel.programming@gmail.com>
2024-09-13 01:47:38 -0600

    bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 10:44:30 2024
+
+@author: h4ckxel
+"""
+
+
+def bubble_sort(arreglo):
+    n = len(arreglo)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arreglo[j] > arreglo [j+1]:
+                #arreglo[j], arreglo[j+1] = arreglo[j+1], arreglo[j]
+                aux = arreglo [j]
+                arreglo[j] = aux
+                arreglo[j] = arreglo[j+1]
+                arreglo[j+1] = aux
+            print(f"Paso: {j+1}")
+            print(f"Se intercambia {arreglo[j]} por  {arreglo[j+1]}")
+            print(arreglo)
+        print()
+    
+    
+# arreglo ejemplo
+huarache = [7,3,9,11,1,0,8]
+print("Arreglo original: ", huarache)
+
+# ordenar arreglo
+bubble_sort(huarache)
+print("Arreglo ordenado es: ", huarache)
