ae087d875464d9c8f0b237e94bbfe17cf337a5ef
h4ckxel <acxel.programming@gmail.com>
2024-09-23 12:17:42 -0600

    Scripts-Ejercicios/Python/Ordenamientos-general.py

--- Scripts-Ejercicios/Python/Ordenamientos-general.py
+++ Scripts-Ejercicios/Python/Ordenamientos-general.py
@@ -4,7 +4,7 @@ from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 from insertion_aort import insertion_sort as injerto
 
-huarache = [random.randint(1, 1000) for _ in range(10000)]
+huarache = [random.randint(1, 1000) for _ in range(1000)]
 
 # ordenamiento burbuja
 inicio = time.time()
@@ -18,14 +18,14 @@ selection_sort(huarache)
 fin = time.time()
 print(f"\nOrdenamiento por Selection Sort. Tiempo: {fin - inicio: .10f} segundos")
 
-# ordenamiento burbuja
+# ordenamiento sorted python
 inicio = time.time()
 sorted(huarache)
 fin = time.time()
 print(f"\nOrdenamiento por Sorted(Python). Tiempo: {fin - inicio: .10f} segundos")
 
-# ordenamiento burbuja
+# ordenamiento injerto
 inicio = time.time()
 injerto(huarache)
 fin = time.time()
-print(f"\nOrdenamiento por Sorted(Python). Tiempo: {fin - inicio: .10f} segundos")
+print(f"\nOrdenamiento por Injerto. Tiempo: {fin - inicio: .10f} segundos\n")
