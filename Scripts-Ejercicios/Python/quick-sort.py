113aa9e4f698176cf5bffced8889d4f405dc7cb4
h4ckxel <acxel.programming@gmail.com>
2024-10-03 10:29:19 -0600

    added by h4ckxel

---
+++ Scripts-Ejercicios/Python/quick-sort.py
@@ -0,0 +1,34 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) - 1]
+
+    i = 0
+    for j in range(len(arr)-1):
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[i], arr[len(arr)-1] = arr[len(arr)-1], arr[i]
+
+    quicksort(arr[0:i])
+    quicksort(arr[i+1:len(arr)])
+
+    return arr
+
+def partition(arr, pivot):
+
+    i = 0
+    for j in range(len(arr)):
+        if arr[j] < pivot:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+            arr[i], arr[len(arr)-1] = arr[len(arr)-1], arr[i]
+            return i
+
+arr = [4, 2, 7, 3, 1, 6]
+
+print(quicksort(arr))
+
+# Output: [1, 2, 3, 4, 6, 7]
