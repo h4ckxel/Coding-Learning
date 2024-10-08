b887c16f111bcecf1c53205ce1edce87a1377eeb
h4ckxel <acxel.programming@gmail.com>
2024-10-08 11:25:17 -0600

    added code by h4ckxel

--- Scripts-Ejercicios/Python/pilas.py
+++ Scripts-Ejercicios/Python/pilas.py
@@ -6,7 +6,7 @@ class Nodo:
 
 # hacer nuevos nodos va a proveer las operaciones de pilas
 class Pila:
-    def __init__(self, dato):
+    def __init__(self):
         self.superior = None
     
     def push(self, dato):
@@ -17,3 +17,33 @@ class Pila:
         nuevo_nodo = Nodo(dato)
         nuevo_nodo.siguiente = self.superior
         self.superior = nuevo_nodo
+    
+    # si no hay datos en el nodo superior, regresamos
+    def pop(self):
+        if self.superior == None:
+            print("La pila esta vacia")
+            return 0
+        print(f"Desapilar {self.superior.dato}")
+        self.superior = self.superior.siguiente
+    
+    def peek(self):
+        print("Imprimiendo la Pila: ")
+        # se recorre la pila y se imprimen valores
+        nodo_temporal = self.superior
+        while nodo_temporal != None:
+            print(f"{nodo_temporal.dato}", end=",")
+            print("")
+            nodo_temporal = nodo_temporal.siguiente
+
+# uso de la pila
+pila = Pila()
+pila.push("Jenny Rivera")
+pila.push("El peje")
+pila.push("Vladimir Putin")
+pila.peek()
+pila.pop()
+pila.peek()
+pila.push("Leon S. Kennedy")
+pila.peek()
+pila.pop()
+pila.peek()
