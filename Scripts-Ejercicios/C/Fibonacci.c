2a329aca861b06a74aaad8b99e7def36e8bc30d4
h4ckxel <acxel.programming@gmail.com>
2024-10-07 21:30:09 -0600

    edit Fibonacci

--- Scripts-Ejercicios/C/Fibonacci.c
+++ Scripts-Ejercicios/C/Fibonacci.c
@@ -3,18 +3,18 @@
 
 int main(){
 	int a,b,c,n,i;
-  	a=0;
-  	b=1;
- 
- 	printf("Digita hasta que numero quieres calcular la serie de Fibonacci: ");
- 	scanf("%d", &n);
- 	printf("%d %d ", a, b);
- 
+	a=0;
+	b=1;
+
+	printf("Digita hasta que numero quieres calcular la serie de Fibonacci: ");
+	scanf("%d", &n);
+	printf("%d %d ", a, b);
+
 	for (i=1;i<=n;i++){
-      c=a+b;
-      printf("%d ",c);
-      a=b;
-      b=c;
-  	}     
-  return 0;
+    c=a+b;
+    printf("%d ",c);
+    a=b;
+    b=c;
+	}     
+return 0;
 }
