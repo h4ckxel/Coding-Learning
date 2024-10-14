152d38250ad7aec09ea24aa7ded4e7b36384e9a8
h4ckxel <acxel.programming@gmail.com>
2024-10-14 01:50:13 -0600

    Added new script

---
+++ Scripts-Ejercicios/Python/POOsimple.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
+    def __init__(self, name, age, career):
+        super().__init__(name, age)
+        self.career = career
+
+def info(Student):
+    print(f"Name: {Student.name}")
+    print(f"Age: {Student.age}")
+    print(f"Career: {Student.career}")
+
+student = Student("Acxel", 23, "Software Engineering")
+info(student)
