e5c1d410ae332b7dd07e3b36da980bba2c5b75c2
h4ckxel <acxel.programming@gmail.com>
2024-11-19 22:59:15 -0600

    added: new scripts in /Python

--- Scripts-Ejercicios/Python/manim.py
+++ Scripts-Ejercicios/Python/manim.py
@@ -1,6 +1,8 @@
 from manim import *
 
-class Example(Scene):
-    def constructor(self):
-        self.play(FadeIn(Dot(Radius=4, color=WHITE)))
-        self.wait(3)
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
